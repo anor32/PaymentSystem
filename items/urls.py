b17bf16235c6ff584655d67297ef123c19d11313
anor23 <burgchiken@gmail.com>
2025-12-22 20:08:58 +0300

    исправлено имя

--- items/urls.py
+++ items/urls.py
@@ -5,5 +5,5 @@ from items.views import ItemView, BuyView
 
 app_name = PaymentsConfig.name
 urlpatterns = [path('item/<int:pk>', ItemView.as_view(), name='item'),
-               path('buy/',BuyView.as_view(), name='buy'),
+               path('buy/<int:pk>',BuyView.as_view(), name='buy_item'),
                ]
