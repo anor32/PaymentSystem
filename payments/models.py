271358362adb34c0f01d4eb4440d9010bffbeba3
anor23 <burgchiken@gmail.com>
2025-12-22 11:22:38 +0300

    созданы первые модели добавлено приложение в настройки

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=100,verbose_name="Имя")
+    description = models.TextField(max_length=500, verbose_name='Описание')
+    price = models.FloatField()
+
+    def __str__(self):
+            return self.name
+
+class Order(models.Model):
+    items = models.ManyToManyField(Item)
