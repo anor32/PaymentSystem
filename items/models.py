71326a64a364f8f4e3ec22e8028046adca3a9c2f
anor23 <burgchiken@gmail.com>
2025-12-23 22:03:21 +0300

    добавлена новая модель для заказа и представения

--- items/models.py
+++ items/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.forms import ModelChoiceField
+from django.utils.regex_helper import Choice
+
+from users.models import User
+
 
 # Create your models here.
 class Item(models.Model):
@@ -6,8 +11,42 @@ class Item(models.Model):
     description = models.TextField(max_length=500, verbose_name='Описание')
     price = models.FloatField()
 
+
     def __str__(self):
             return self.name
 
+class OrderItem(models.Model):
+    order = models.ForeignKey('Order', on_delete=models.CASCADE, related_name='order_items')
+    item = models.ForeignKey(Item, on_delete=models.PROTECT)
+    quantity = models.IntegerField(default=1)
+
+    @property
+    def total_price(self):
+        return self.item.price * self.quantity
+
+    def __str__(self):
+        return self.item.name + ' '+str(self.quantity)
+
+
+
 class Order(models.Model):
-    items = models.ManyToManyField(Item)
+    class Status(models.TextChoices):
+        PENDING = 'Ожидает оплаты'
+        PAID =  'Оплачен'
+        SHIPPED = 'Отправлен'
+        DELIVERED =  'Доставлен'
+        CANCELLED =  'Отменен'
+
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=15, choices=Status.choices, default=Status.PENDING)
+
+
+    @property
+    def total_price(self):
+        return sum([order_item.total_price for order_item in self.items.all()])
+
+
+    @property
+    def items(self):
+        return OrderItem.objects.filter(order=self)
--- items/views.py
+++ items/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.views.generic import CreateView, DetailView, ListView
 import stripe
-from items.models import Item
+from items.models import Item, Order, OrderItem
 
 
 # Create your views here.
@@ -18,27 +18,52 @@ class ItemsView(ListView):
     template_name = 'items.html'
 
 
-
-
-
 class BuyView(View):
-    model = Item
+    model = Order
     template_name = 'success_page.html'
 
+    def get(self, request, pk):
+        order = get_object_or_404(Order, pk=pk)
+        prices = []
 
-    def get(self,request,pk):
-        item = get_object_or_404(Item, pk=pk)
-        product = stripe.Product.create(
-            name=item.name,
-            default_price_data={"unit_amount": int(item.price), "currency": "usd"},
-            expand=["default_price"],
-        )
+        for item in order.items.all():
+            product = stripe.Product.create(
+                name=item.name,
+                default_price_data={"unit_amount": int(item.price), "currency": "usd"},
+                expand=["default_price"],
+            )
 
-        price = product.default_price.id
+            prices.append({'price': product.default_price.id, 'quantity': product.quantity})
+
         session = stripe.checkout.Session.create(
             success_url="http://localhost:8000/success_page.html",
-            line_items=[{"price": price, "quantity": 1}],
+            line_items=prices,
             mode="payment",
         )
 
         return JsonResponse({'session': session})
+
+
+class OrderView(View):
+    model = Order
+    template_name = 'cart.html'
+
+    def get(self, request, pk):
+        order = get_object_or_404(Order, pk=pk)
+        items = order.items.all().prefetch_related()
+
+        return render(request, 'cart.html', {'order': order, 'items': items})
+
+    def post(self, request, pk):
+        item = get_object_or_404(Item, pk=pk)
+        order = Order.objects.create(user=request.user)
+        OrderItem.objects.create(item=item, quantity=1, order=order)
+        self.request.session['order_id'] = order.pk
+        return JsonResponse({'order_id': order.pk})
+
+    def update(self, request, pk):
+        item = get_object_or_404(Order, pk=pk)
+        order_item = get_object_or_404(OrderItem, item=item)
+        order_item.quantity = order_item.quantity + 1
+        order_pk = self.request.session['order_id']
+        return JsonResponse({'order_id': order_pk})
