41603231b878bd7d52a206bfcb423b44d5cb628e
anor23 <burgchiken@gmail.com>
2025-12-23 22:03:02 +0300

    обновлена админка новые модели

--- items/admin.py
+++ items/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from items.models import Item, Order
+from items.models import Item, Order, OrderItem
 
 
 # Register your models here.
@@ -11,4 +11,20 @@ class ItemAdmin(admin.ModelAdmin):
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('item', 'quantity', 'price')
+    list_display = ('id','order_items','total_price','user')
+
+
+
+    def order_items(self, obj):
+        result_string = ''
+        for item in obj.items.all():
+            result_string +=   item.item.name + '  '+ str(item.quantity) + 'шт '
+
+        return result_string
+
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('id','item','quantity')
+
