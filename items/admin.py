88aaf70a69cbe8b0583c78e9b98e4806a109660a
anor23 <burgchiken@gmail.com>
2025-12-23 15:04:05 +0300

    добавлена новая модель в админку

--- items/admin.py
+++ items/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from items.models import Item
+from items.models import Item, Order
 
 
 # Register your models here.
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
     list_display = ('name', 'description','price')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('item', 'quantity', 'price')
