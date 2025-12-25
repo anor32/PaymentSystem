8a454730e965f112a6102c01c38f6d6defd2df48
anor23 <burgchiken@gmail.com>
2025-12-25 10:46:23 +0300

    настройки поиска путей улучшены

--- config/settings.py
+++ config/settings.py
@@ -60,7 +60,11 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+            BASE_DIR / 'users' / 'templates',
+
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
