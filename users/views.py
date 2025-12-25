bb85d24a66d59cca55147f5de79dcfefab8a3c44
anor23 <burgchiken@gmail.com>
2025-12-25 10:47:22 +0300

    view для users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+from users.models import User
+
 
 # Create your views here.
+
+
+class HomePageView(TemplateView):
+    model = User
+    template_name = 'home.html'
+
+
+
+
+
+class UserLogoutView(TemplateView):
+    model = User
+    success_url = '/'
+
+class UserRegisterView(TemplateView):
+    template_name = 'users/register.html'
