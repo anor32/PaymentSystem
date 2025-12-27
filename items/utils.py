a57f4d7549e64ac6915c33526d2a765043096a0d
anor23 <burgchiken@gmail.com>
2025-12-27 19:00:22 +0300

    фикс багов

--- items/utils.py
+++ items/utils.py
@@ -9,10 +9,15 @@ def check_discounts_and_tax(order):
             display_name=order.tax.name if order.tax else "Discount Tax",
             percentage=order.tax.percent,
             inclusive=False, )
+        tax_percent = [tax_percent.id]
+
     if order.discount:
         coupon = stripe.Coupon.create(
             percent_off=order.discount.percent,
             duration="once",
         )
+        coupon = [{'coupon': coupon.id}]
 
-    return [{'coupon': coupon.id}], [tax_percent.id]
+
+
+    return coupon, tax_percent
