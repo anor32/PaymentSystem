25799483130bd5bdd52125e7c2a6e418df4a2f89
anor23 <burgchiken@gmail.com>
2025-12-31 02:04:26 +0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py loaddata fixtures/payments.json
 python manage.py loaddata fixtures/users.json
-exec gunicorn myapp.wsgi:application --bind 0.0.0.0:10000
+exec gunicorn config.wsgi:application --bind 0.0.0.0:10000
