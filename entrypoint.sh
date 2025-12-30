cef79f7cb66cac2c5631042d046bd6fe71565ff8
anor23 <burgchiken@gmail.com>
2025-12-31 02:29:39 +0300

    изменен скрипт

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+set -e
+
+mkdir -p /code/staticfiles
+
 python manage.py makemigrations
 python manage.py migrate
-python manage.py loaddata fixtures/payments.json
-python manage.py loaddata fixtures/users.json
+python manage.py loaddata payments.json users.json --ignore
+
 python manage.py collectstatic --noinput
-exec gunicorn config.wsgi:application --bind 0.0.0.0:10000
+
+exec gunicorn your_project.wsgi:application --bind 0.0.0.0:10000
